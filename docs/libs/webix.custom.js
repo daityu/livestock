9d4f1f8eb4341848b774e26e0e151cd6ad944fac
k_oonaka <k_oonaka@n-c-c.co.jp>
2022-07-14 13:26:09 +0900

    保存機能追加

--- docs/libs/webix.custom.js
+++ docs/libs/webix.custom.js
@@ -1,30 +1,42 @@
 webix.protoUI({
-    name: "nproperty",
-    $cssName: "property",
+    name: "ndatepicker",
+    $cssName: "datepicker",
     defaults: {
+        stringResult:true
     },
-    $init: function (config) {
-        this.attachEvent("onAfterEditStart", function () {//html5 type設定
-            var hash = this.getEditor().config.attr;
-            if (this.getEditor().config.attr != {}) {
-                Object.keys(hash).map(key => this.getEditor().getInputNode().setAttribute(key, hash[key]));
-            }
-        });
-    }
-}, webix.ui.property, webix.ValidateData);
+}, webix.ui.datepicker);
+// webix.protoUI({
+//     name: "nproperty",
+//     $cssName: "property",
+//     defaults: {
+//     },
+//     $init: function (config) {
+//         this.attachEvent("onAfterEditStart", function () {//html5 type設定
+//             var hash = this.getEditor().config.attr;
+//             if (this.getEditor().config.attr != {}) {
+//                 Object.keys(hash).map(key => this.getEditor().getInputNode().setAttribute(key, hash[key]));
+//             }
+//         });
+//     }
+// }, webix.ui.property, webix.ValidateData);
 webix.protoUI({
     name: "yen",
     $cssName: "text",
     defaults: {
-        format:"1,111",
-        attributes:{
-            type:"text",
-            inputmode:"tel"
+        format: "1,111",
+        attributes: {
+            type: "text",
+            inputmode: "tel"
         }
     },
     $init: function (config) {
         this.attachEvent("onFocus", function () {//html5 type設定
             this.getInputNode().select();
+        });
+        this.attachEvent("onBlur", function () {//変換エラー時は削除
+            if (this.getValue() == "NaN") {
+                this.setValue("");
+            }
         });
     }
 }, webix.ui.text);
