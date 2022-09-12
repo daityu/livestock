db5060902e9a1ec302cc6e70a5a4ba8022bd0a5c
k_oonaka <k_oonaka@n-c-c.co.jp>
2022-09-12 09:48:33 +0900

    monthLayout追加

--- docs/libs/NCC.js
+++ docs/libs/NCC.js
@@ -3,18 +3,39 @@ NCC.datepicker = NCC.datepicker || {};
 // datepickerの日付を加減算する
 // @param string datepickerId datepickerのID
 // @param int addDay 加算する日付（マイナス可）
-NCC.datepicker.addDate = function(datepickerId, addDay){
-  if(!datepickerId || !addDay) return;
-  var date  = new Date($$(datepickerId).getValue());
-  var _nd   = Conversion.formatDate(date.setDate(date.getDate() + parseInt(addDay, 10)), "YYYY/MM/DD");
+NCC.datepicker.addDate = function (datepickerId, addDay) {
+  if (!datepickerId || !addDay) return;
+  var date = new Date($$(datepickerId).getValue());
+  var _nd = Conversion.formatDate(date.setDate(date.getDate() + parseInt(addDay, 10)), "YYYY/MM/DD");
   $$(datepickerId).setValue(_nd);
 };
 // datepickerの年月を加減算する
 // @param string datepickerId datepickerのID
 // @param int addMonth 加算する年月（マイナス可）
-NCC.datepicker.addMonth = function(datepickerId, addMonth){
-  if(!datepickerId || !addMonth) return;
-  var date  = new Date($$(datepickerId).getValue());
-  var _nd   = Conversion.formatDate(date.setMonth(date.getMonth() + parseInt(addMonth, 10)), "YYYY/MM/1");
+NCC.datepicker.addMonth = function (datepickerId, addMonth) {
+  if (!datepickerId || !addMonth) return;
+  var date = new Date($$(datepickerId).getValue());
+  var _nd = Conversion.formatDate(date.setMonth(date.getMonth() + parseInt(addMonth, 10)), "YYYY/MM/1");
   $$(datepickerId).setValue(_nd);
 };
+NCC.datepicker.monthLayout = function (_id, _param) {
+  _id = _id || "";
+  _param = _param || {};
+  var _obj = {
+    cols: [
+      {
+        view: "button", type: "icon", icon: "wxi-angle-left", width: 60, click: function (id, event) {
+          NCC.datepicker.addMonth(_id, -1);
+        }, tooltip: "前月へ"
+      },
+      { view: "ndatepicker", id: _id, type: "month", format: "%Y年%m月", value: new Date(), width: 200 },
+      {
+        view: "button", type: "icon", icon: "wxi-angle-right", width: 60, click: function (id, event) {
+          NCC.datepicker.addMonth(_id, 1);
+        }, tooltip: "翌月へ"
+      },
+      {}
+    ]
+  }
+  return _obj;
+}
