36990b963e593eaf0e125b416b31e249a5e27c6c
k_oonaka <k_oonaka@n-c-c.co.jp>
2022-10-14 16:39:29 +0900

    登録月変更

--- docs/libs/NCC.js
+++ docs/libs/NCC.js
@@ -18,19 +18,22 @@ NCC.datepicker.addMonth = function (datepickerId, addMonth) {
   var _nd = Conversion.formatDate(date.setMonth(date.getMonth() + parseInt(addMonth, 10)), "YYYY/MM/1");
   $$(datepickerId).setValue(_nd);
 };
-NCC.datepicker.monthLayout = function (_id, _param) {
+NCC.datepicker.monthLayout = function (_id, _label, _param) {
   _id = _id || "";
+  _label = _label || "";
+  let _labelWidth = _label.length * 20;
   _param = _param || {};
   var _obj = {
     cols: [
+      { view: "label", label: _label, width:_labelWidth, align:"right" },
       {
-        view: "button", type: "icon", icon: "wxi-angle-left", width: 60, click: function (id, event) {
+        view: "button", type: "icon", icon: "wxi-angle-left", width: 45, click: function (id, event) {
           NCC.datepicker.addMonth(_id, -1);
         }, tooltip: "前月へ"
       },
       { view: "ndatepicker", id: _id, type: "month", format: "%Y年%m月", value: new Date(), width: 200 },
       {
-        view: "button", type: "icon", icon: "wxi-angle-right", width: 60, click: function (id, event) {
+        view: "button", type: "icon", icon: "wxi-angle-right", width: 45, click: function (id, event) {
           NCC.datepicker.addMonth(_id, 1);
         }, tooltip: "翌月へ"
       },
